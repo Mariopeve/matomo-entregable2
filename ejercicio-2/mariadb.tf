80241ce59e40e251ce829a95ecf7c8d13ac4eb64
PC GAMING <mariopeve5971@gmail.com>
2025-12-05 12:08:50 +0100

    Añadidos deployments Matomo + MariaDB + PVCs

---
+++ ejercicio-2/mariadb.tf
@@ -0,0 +1,81 @@
+resource "kubernetes_deployment" "mariadb" {
+  metadata {
+    name = "mariadb"
+    labels = {
+      app = "mariadb"
+    }
+  }
+
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        app = "mariadb"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "mariadb"
+        }
+      }
+      spec {
+        container {
+          name  = "mariadb"
+          image = "mariadb:latest"
+          port {
+            container_port = 3306
+          }
+
+          env {
+            name  = "MYSQL_ROOT_PASSWORD"
+            value = var.mysql_root_password
+          }
+          env {
+            name  = "MYSQL_DATABASE"
+            value = var.mysql_database
+          }
+          env {
+            name  = "MYSQL_USER"
+            value = var.mysql_user
+          }
+          env {
+            name  = "MYSQL_PASSWORD"
+            value = var.mysql_password
+          }
+
+          volume_mount {
+            name      = "mariadb-storage"
+            mount_path = "/var/lib/mysql"
+          }
+        }
+
+        volume {
+          name = "mariadb-storage"
+          persistent_volume_claim {
+            claim_name = kubernetes_persistent_volume_claim.mariadb_pvc.metadata[0].name
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "mariadb_service" {
+  metadata {
+    name = "service-mariadb"
+    labels = {
+      app = "mariadb"
+    }
+  }
+  spec {
+    selector = {
+      app = "mariadb"
+    }
+    port {
+      port        = 3306
+      target_port = 3306
+    }
+    type = "ClusterIP"
+  }
+}
---
+++ ejercicio-2/matomo.tf
@@ -0,0 +1,79 @@
+resource "kubernetes_deployment" "matomo" {
+  metadata {
+    name = "matomo"
+    labels = {
+      app = "matomo"
+    }
+  }
+
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        app = "matomo"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "matomo"
+        }
+      }
+      spec {
+        container {
+          name  = "matomo"
+          image = var.matomo_image
+          port {
+            container_port = 80
+          }
+
+          env {
+            name  = "MATOMO_DATABASE_HOST"
+            value = "service-mariadb"
+          }
+          env {
+            name  = "MATOMO_DATABASE_USERNAME"
+            value = var.mysql_user
+          }
+          env {
+            name  = "MATOMO_DATABASE_PASSWORD"
+            value = var.mysql_password
+          }
+          env {
+            name  = "MATOMO_DATABASE_DBNAME"
+            value = var.mysql_database
+          }
+
+          volume_mount {
+            name      = "matomo-storage"
+            mount_path = "/var/www/html"
+          }
+        }
+
+        volume {
+          name = "matomo-storage"
+          persistent_volume_claim {
+            claim_name = kubernetes_persistent_volume_claim.matomo_pvc.metadata[0].name
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "matomo_service" {
+  metadata {
+    name = "service-matomo"
+  }
+  spec {
+    selector = {
+      app = "matomo"
+    }
+    type = "NodePort"
+    port {
+      port       = 80
+      node_port  = 30081
+      target_port = 80
+    }
+  }
+}
